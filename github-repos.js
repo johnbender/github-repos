ddbfdd9d00f9507de6453cae5ebfafa12165a9a6
John Bender <john.m.bender@gmail.com>
2008-11-29 15:03:14 -0500

    Added my name in the code so credit is given and a license link.

--- github-repos.js
+++ github-repos.js
@@ -1,3 +1,8 @@
+//Written by John Bender for http://erlanguid.com
+//See: http://nickelcode.com/2008/11/27/github-repo-information-in-your-webpages/
+//for an explanation of implementation and how to use it 
+//released under the GPL v2
+//http://www.gnu.org/licenses/gpl-2.0.txt
 GITHUB_JSON_URL = "http://github.com/api/v1/json/{0}?callback={1}";
 DYNAMIC_SCRIPT_TEMPLATE = "<script src='{0}' type='text/javascript' ></script>";
 GITHUB_PROJECTS_CLASS = ".github-projects";
@@ -10,9 +15,8 @@ REPO_TEMPLATE =
     +"</div>";
 
 var GitHubRepos = new function(){
-
     this.displayObjects = new Array();
-
+    
     //retrieve the repo information from github    
     this.GetRepos = function()
     {
@@ -22,11 +26,9 @@ var GitHubRepos = new function(){
 
         //for all the github-projects divs 
         $(GITHUB_PROJECTS_CLASS).each(function(i) {
-
             //create an array of all unique userids in the page
             if(this.id != undefined)
             {
-
                 var username = this.id;
                 //add this object (div by default) that met the query parameters
                 //to a list to be altered later
@@ -34,25 +36,19 @@ var GitHubRepos = new function(){
                 {
                    GitHubRepos.displayObjects[username] = new Array(); 
                 }
-
                 GitHubRepos.displayObjects[username][i] = this;
             }
-
         });   
-
         //for each unique user
         for(var i in GitHubRepos.displayObjects)
         {
             //format the github url for our given user and callback function
             var github_url = format(GITHUB_JSON_URL, i, "GitHubRepos.DisplayRepos");
-
             //insert a script into the DOM to add our JSON data via the github url
             //once the script loads it will callback the DATA_DISPLAY_FUCNTION
             $('head').append(format(DYNAMIC_SCRIPT_TEMPLATE, github_url));
-
         }
     }
-
     //add the repo data to the div using the template
     this.DisplayRepos = function(data)
     {
@@ -60,29 +56,22 @@ var GitHubRepos = new function(){
         if(data.user != undefined)
         {
             var user = data.user;
-
             //for all the div objects that correspond to that user
             for(var j in GitHubRepos.displayObjects[user.login])
             {
                 var proj_div = GitHubRepos.displayObjects[user.login][j];
-                 
                 //for each of the user repositories in the data
                 $.each(user.repositories, function(i, repo) {
-
                     //add the templated information to the divs existing content
                     proj_div.innerHTML = proj_div.innerHTML + format(REPO_TEMPLATE, repo.url, repo.name);
-              
                 });
-
                 //the style div.github-projects is display: none by default
                 //display it if there was no error message
                 proj_div.style.display = "block";
             }
-            
         }
     } 
 }
-
 //used to create clean insertion of strings into templates
 function format(str)
 {
@@ -92,14 +81,5 @@ function format(str)
     }
     return str;
 }
-
 //Go Get Those REPOS!
-$(document).ready(function(){ 
-
-    GitHubRepos.GetRepos();
-
-});
-
-
-
-
+$(document).ready(function(){GitHubRepos.GetRepos();});
